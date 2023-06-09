1c0a6249ef4f4ffc79e655ac034a6e80d69fb801
JoanaOP <joana.orpella@gmail.com>
2023-06-09 10:24:36 +0200

    go to name working

--- src/views/FollowMePage.vue
+++ src/views/FollowMePage.vue
@@ -20,19 +20,23 @@
             <ion-label>{{ name }}</ion-label>
           </ion-item>
         </ion-list>
+        <ion-button color="tertiary" @click="speak">
+          <ion-icon slot="icon-only" :icon="micOutline"></ion-icon>
+        </ion-button>
       </ion-content>      
     </ion-page>
 </template>
   
 <script>
   import { defineComponent, onMounted, ref, provide } from 'vue';
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonLabel, IonList, IonItem, alertController  } from '@ionic/vue';
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonLabel, IonList, IonItem, alertController, IonIcon, IonButton } from '@ionic/vue';
+  import { micOutline } from 'ionicons/icons';
   import { useMQTT } from 'mqtt-vue-hook'
   import { useRouter, useRoute } from 'vue-router'
   
   export default  defineComponent({
     name: 'FollowMePage',
-    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonText, IonLabel, IonList, IonItem },
+    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonText, IonLabel, IonList, IonItem, IonIcon, IonButton },
     ionViewDidEnter(){
       this.username = this.$route.params.player;
     },
@@ -45,6 +49,11 @@
       let username = ref(undefined);
       let yourTurn = ref(false)
       let interval;
+      let listenning = false;
+
+      const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const sr = new Recognition();
+      sr.lang = "es-US";
 
       const yourTurnAlert = async () => {
         const alert = await alertController.create({
@@ -52,34 +61,71 @@
           subHeader: "It's your turn!",
           buttons: ['OK'],
         });
-
         await alert.present();
       };
-      
+
+      const sameNameAlert = async () => {
+        const alert = await alertController.create({
+          header: 'Alert',
+          subHeader: "You can't say your own name",
+          buttons: ['OK'],
+        });
+        await alert.present();
+      };
+            
 
       onMounted(() => {       
         
         mqttHook.subscribe('+/mobileApp/#',1 )
         mqttHook.subscribe('autopilotService/dashboardFollowme/waypointReached')
+        mqttHook.subscribe('mobileApp/dashboardFollowme/following')
         mqttHook.publish('mobileApp/dashboardFollowme/sendList','')
 
         sendCoordinates();
 
+        sr.continuous = true;
+        sr.interimResults = true;
+
+        sr.onstart = () => {
+          console.log('SR Started');
+          listenning = true;
+        }
+
+        sr.onend = () => {
+          console.log('SR Stopped');
+          listenning = false;
+        }
+
+        sr.onresult = (res)  => {
+          for (let i = 0; i < res.results.length; i++) {
+            const result = res.results[i]
+            if (result.isFinal){
+              checkForPlayer(result);
+            } 
+          }
+          //let t = Array.from(res.results).map(result => result[0]).map(result => result.transcript);
+        };
+
         mqttHook.registerEvent("+/mobileApp/#", (topic, message)=>{
           if(topic=='dashboardFollowme/mobileApp/updateList'){
-            players.value = JSON.parse(message);
+            let data = JSON.parse(message);
+            players.value = data.players;
           }
-          else if(topic=='dashboardFollowme/mobileApp/following'){
+          else if(topic=='dashboardFollowme/mobileApp/yourTurn/'+username.value){
+            yourTurn.value = true;
+          }                    
+        })
+
+        mqttHook.registerEvent("+/dashboardFollowme/#", (topic, message)=>{
+          console.log('received')
+          if(topic=='mobileApp/dashboardFollowme/following'){
             followingName.value = message.toString();
             if(followingName.value == username.value){
               followingName.value = 'you!'
             }
+            yourTurn.value = false
           }
-        })
-
-        mqttHook.registerEvent("autopilotService/dashboardFollowme/waypointReached", (topic, message)=>{
-          console.log('received')
-          if(topic=='autopilotService/dashboardFollowme/waypointReached'){
+          else if(topic=='autopilotService/dashboardFollowme/waypointReached'){
             if(followingName.value == 'you!'){
               yourTurn.value = true;
               yourTurnAlert()
@@ -107,13 +153,13 @@
             ) */
 
             //fem aixo per fer veure que tinc la posicio
-            if(username.value == 'p1'){
+            if(username.value == 'joana'){
               position = p1wp[a]
             }
-            else if(username.value == 'p2'){
+            else if(username.value == 'victor'){
               position = p2wp[a]
             }
-            else if(username.value == 'p3'){
+            else if(username.value == 'mama'){
               position = p3wp[a]
             }
             a = a + 1;
@@ -134,13 +180,47 @@
           console.log ('no hay geolocation')
         }
       }
+
+      function checkForPlayer(result){
+        const t = result[0].transcript;
+        console.log(t.toLowerCase())
+
+        for(let i = 0; i < players.value.length; i++){
+          if (t.toLowerCase().trim().includes(players.value[i])) {
+            if(players.value[i] != username.value){
+              mqttHook.publish('mobileApp/dashboardFollowme/following',players.value[i])
+              
+              sr.stop()
+            }
+            else{
+              sameNameAlert();
+              sr.stop()
+            }
+          }
+        }
+        
+      }
+
+      let a = 1
+
+      function speak(){
+        if(listenning == false){
+          sr.start()
+        }        
+       /*  mqttHook.publish('mobileApp/dashboardFollowme/following',players.value[a])
+        console.log(players.value[a])
+        a = a - 1 */
+      }
       
       return {
         followingName,
         players,
         yourTurn,
         username,
-        yourTurnAlert
+        yourTurnAlert,
+        micOutline,
+        speak,
+        sameNameAlert
       }
     }
   })
